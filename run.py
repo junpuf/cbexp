db3cf270a0a34877a5fb728b5bc3cee2d74fd805
Junpu Fan <junpu@amazon.com>
2020-11-22 23:10:27 +0000

    try log

---
+++ run.py
@@ -0,0 +1,47 @@
+import logging
+import sys
+import argparse
+import os
+
+def main():
+    if os.path.exists(args.log_filepath):
+        os.remove(args.log_filepath)
+
+    args = parse_args()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(message)s')
+    # file logging
+    logfile_handler = logging.FileHandler(args.log_filepath)
+    logfile_handler.setFormatter(formatter)
+    logger.addHandler(logfile_handler)
+    # stdout logging
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
+    # stderr logging
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setFormatter(formatter)
+    logger.addHandler(stderr_handler)
+
+    # log some stuff
+    for i in range(1000):
+        logger.warning("hello world")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log-filepath",
+        default=None,
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        pass
