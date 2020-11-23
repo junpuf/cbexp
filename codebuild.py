85e52eb8022013e2539b9f5cfed16e01e82e3cba
Junpu Fan <junpu@amazon.com>
2020-11-23 00:13:21 +0000

    fix override type

--- codebuild.py
+++ codebuild.py
@@ -18,10 +18,10 @@ def main():
     source_version = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
     build_commands = "python3 run.py --log-filepath \"${CODEBUILD_SRC_DIR}/.tmp/build_${CODEBUILD_LOG_PATH}.log\""
     build_finally = "aws s3 cp ${CODEBUILD_SRC_DIR}/.tmp/build_${CODEBUILD_LOG_PATH}.log ${JUNPU_S3_LOG_URI}"    
-    start_build(project_name="cbexp", source_version=source_version, env_overrides={
+    start_build(project_name="cbexp", source_version=source_version, env_overrides=[
         {"name": "JUNPU_BUILD_COMMANDS", "value": build_commands, "type": "PLAINTEXT"},
         {"name": "JUNPU_BUILD_FINALLY", "value": build_finally, "type": "PLAINTEXT"}
-    })
+    ])
 
 
 if __name__ == "__main__":
