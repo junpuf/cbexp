136ab04a484a1a423bf33ed47e5f9dec07bd9f85
Junpu Fan <junpu@amazon.com>
2020-11-23 08:35:06 +0000

    add github comment

--- codebuild.py
+++ codebuild.py
@@ -17,7 +17,7 @@ def start_build(project_name, source_version, env_overrides):
 def main():
     source_version = os.getenv("CODEBUILD_RESOLVED_SOURCE_VERSION")
     build_commands = "python3 run.py --tmp-dir \"${CODEBUILD_SRC_DIR}/.tmp\" --log-filename \"build_${CODEBUILD_LOG_PATH}.log\" --build-report-filepath \"${CODEBUILD_SRC_DIR}/.tmp/build_report.json\""
-    build_finally = "aws s3 cp ${CODEBUILD_SRC_DIR}/.tmp/build_${CODEBUILD_LOG_PATH}.log ${JUNPU_S3_LOG_URI}"    
+    build_finally = "aws s3 cp \"{CODEBUILD_SRC_DIR}/.tmp/build_${CODEBUILD_LOG_PATH}.log\" \"${JUNPU_S3_LOG_URI}\"; python3 github_actions.py --commit_sha \"${CODEBUILD_SOURCE_VERSION}\" --comment \"${JUNPU_S3_LOG_URI}build_${CODEBUILD_LOG_PATH}.log\""     
     start_build(project_name="cbexp", source_version=source_version, env_overrides=[
         {"name": "JUNPU_BUILD_COMMANDS", "value": build_commands, "type": "PLAINTEXT"},
         {"name": "JUNPU_BUILD_FINALLY", "value": build_finally, "type": "PLAINTEXT"}
